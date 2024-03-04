b5d10c7a6b8afbd3ef40be74092cc71ac5bf2276
Swapnil Walhekar <108648658+swapnilwalhekar@users.noreply.github.com>
2024-03-04 18:45:12 +0530

    work on auth services

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,7 +1,64 @@
 import config from "../config/config.js";
 import { Client, Account, ID } from "appwrite";
 
-export class AuthService {}
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  // for account creation
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // get current user account
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getcurrentUser :: error", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: getcurrentUser :: error", error);
+    }
+  }
+}
 
 const authService = new AuthService();
 
